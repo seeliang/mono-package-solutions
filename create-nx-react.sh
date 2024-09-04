b253480848c29b18fb4300429132934f923e5f12
sihui-liang-gtg <147350623+sihui-liang-gtg@users.noreply.github.com>
2024-09-04 16:14:15 +1000

    chore: have to use vite to get package json

--- create-nx-react.sh
+++ create-nx-react.sh
@@ -1,3 +1,3 @@
 echo "packages is $1"
 echo "dir is $2"
-nx g @nx/react:library $1 --directory=$2/$1 --linter=none --unitTestRunner=none --bundler=none --style=@emotion/styled
+nx g @nx/react:lib $1 --directory=$2/$1 --linter=none --unitTestRunner=none --bundler=vite --style=@emotion/styled
