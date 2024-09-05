97cbef5540b9831db2a0bca810622805d7e1c47c
sihui-liang-gtg <147350623+sihui-liang-gtg@users.noreply.github.com>
2024-09-05 14:58:31 +1000

    chore: update naming

--- create-nx-react.sh
+++ create-nx-react.sh
@@ -1,3 +1,3 @@
 echo "packages is $1"
 echo "dir is $2"
-nx g @nx/react:lib $1 --directory=$2/$1 --linter=none --unitTestRunner=none --bundler=vite --style=@emotion/styled
+nx g @nx/react:lib @seeliang/mono-react-$1 --directory=$2/$1 --linter=none --unitTestRunner=none --bundler=vite --style=@emotion/styled
