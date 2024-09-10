e0c261d303d9e52ad4cb00ea75155108ee20c7ea
sihui-liang-gtg <147350623+sihui-liang-gtg@users.noreply.github.com>
2024-09-10 10:28:20 +1000

    chore: use script to cover esbuild

--- create-nx-react.sh
+++ create-nx-react.sh
@@ -1,3 +1,11 @@
-echo "packages is $1"
-echo "dir is design"
-nx g @nx/react:lib @seeliang/mono-react-$1 --directory=design/$1 --linter=none --unitTestRunner=none --bundler=vite --style=@emotion/styled
+create-react () {
+    local packages="@seeliang/mono-react-$1"
+    local dir="design/$1"
+    echo "packages is $packages"
+    echo "dir is at $dir"
+    nx g @nx/react:lib $packages --directory=$dir --linter=none --unitTestRunner=none --bundler=none --style=@emotion/styled
+    nx g @nx/esbuild:configuration $packages --platform=browser 
+}
+
+create-react $1
+# nx g @nx/react:lib @seeliang/mono-react-$1 --directory=design/$1 --linter=none --unitTestRunner=none --bundler=none --style=@emotion/styled
